1cf57a555142edbf407ac167d29ddd23b0a8f273
Peter Robinson <drinkcoffee@eml.cc>
2024-10-17 16:48:07 +1000

    Reworked to ensure list of stakers is unique

--- contracts/staking/StakeHolder.sol
+++ contracts/staking/StakeHolder.sol
@@ -3,14 +3,26 @@
 pragma solidity ^0.8.20;
 
 import {UUPSUpgradeable} from "openzeppelin-contracts-upgradeable-4.9.3/proxy/utils/UUPSUpgradeable.sol";
-import {AccessControlEnumerableUpgradeable} from
+import {AccessControlEnumerableUpgradeable, IAccessControlUpgradeable, AccessControlUpgradeable} from
     "openzeppelin-contracts-upgradeable-4.9.3/access/AccessControlEnumerableUpgradeable.sol";
+
+/// @notice Struct to combine an account and an amount.
+struct AccountAmount {
+    address account;
+    uint256 amount;
+}
 
 /**
  * @title StakeHolder: allows anyone to stake any amount of native IMX and to then remove all or part of that stake.
  * @dev The StakeHolder contract is designed to be upgradeable.
  */
 contract StakeHolder is AccessControlEnumerableUpgradeable, UUPSUpgradeable {
+    /// @notice Error: Attempting to upgrade contract storage to version 0.
+    error CanNotUpgradeToV0(uint256 _storageVersion);
+
+    /// @notice Error: Attempting to renounce the last role admin / default admin.
+    error MustHaveOneRoleAdmin();
+
     /// @notice Error: Attempting to stake with zero value.
     error MustStakeMoreThanZero();
 
@@ -20,11 +32,11 @@ contract StakeHolder is AccessControlEnumerableUpgradeable, UUPSUpgradeable {
     /// @notice Error: Attempting to unstake amount greater than the balance.
     error UnstakeAmountExceedsBalance(uint256 _amountToUnstake, uint256 _currentStake);
 
-    /// @notice Error: The length of the receipients and the amounts array did not match.
-    error RecipientsAmountsMismatch(uint256 _receipientsLength, uint256 _amountsLength);
-
     /// @notice Error: The sum of all amounts to distribute did not equal msg.value of the distribute transaction.
     error DistributionAmountsDoNotMatchTotal(uint256 _msgValue, uint256 _calculatedTotalDistribution);
+
+    /// @notice Error: Distributions can only be made to accounts that have staked.
+    error AttemptToDistributeToNewAccount(address _account, uint256 _amount);
 
     /// @notice Event when an amount has been staked or when an amount is distributed to an account.
     event StakeAdded(address _staker, uint256 _amountAdded, uint256 _newBalance);
@@ -38,17 +50,30 @@ contract StakeHolder is AccessControlEnumerableUpgradeable, UUPSUpgradeable {
     /// @notice Only UPGRADE_ROLE can upgrade the contract
     bytes32 public constant UPGRADE_ROLE = bytes32("UPGRADE_ROLE");
 
+    /// @notice Version 0 version number
+    uint256 private constant _VERSION0 = 0;
+
+    /// @notice Holds staking information for a single staker.
+    struct StakeInfo {
+        /// @notice Amount of stake.
+        uint256 stake;  
+        /// @notice True if this account has ever staked.
+        bool hasStaked; 
+    }
+
     /// @notice The amount of value owned by each staker
     // solhint-disable-next-line private-vars-leading-underscore
-    mapping(address staker => uint256 stake) private balances;
+    mapping(address staker => StakeInfo stakeInfo) private balances;
 
     /// @notice A list of all stakers who have ever staked.
-    /// Note that it is possible that this list will contain duplicates if an account stakes,
-    /// then fully unstakes, and then stakes again.
-    /// Not reordering the list of stakers means that off-chain services could cache results,
-    /// thus only needing to fetch new entries in the array.
+    /// @dev The list make contain stakers who have completely unstaked (that is, have 
+    ///    a balance of 0). This array is never re-ordered. As such, off-chain services
+    ///    could cache the results of getStakers().
     // solhint-disable-next-line private-vars-leading-underscore
     address[] private stakers;
+
+    /// @notice version number of the storage variable layout.
+    uint256 public version;
 
     /**
      * @notice Initialises the upgradeable contract, setting up admin accounts.
@@ -60,6 +85,35 @@ contract StakeHolder is AccessControlEnumerableUpgradeable, UUPSUpgradeable {
         __AccessControl_init();
         _grantRole(DEFAULT_ADMIN_ROLE, _roleAdmin);
         _grantRole(UPGRADE_ROLE, _upgradeAdmin);
+        version = _VERSION0;
+    }
+
+    /**
+     * @notice Function to be called when upgrading this contract.
+     * @dev Call this function as part of upgradeToAndCall().
+     *      This initial version of this function reverts. There is no situation 
+     *      in which it makes sense to upgrade to the V0 storage layout.
+     *      Note that this function is permissionless. Future versions must 
+     *      compare the code version and the storage version and upgrade 
+     *      appropriately. As such, the code will revert if an attacker calls 
+     *      this function attempting a malicious upgrade.
+     * @ param _data ABI encoded data to be used as part of the contract storage upgrade.
+     */
+    function upgradeStorage(bytes memory /* _data */) external view {
+        revert CanNotUpgradeToV0(version);
+    }
+
+    /**
+     * @notice Renounce a role assigned to msg.sender.
+     * @dev Prevents the last default admin role from renouncing the role.
+     * @param _role The role to be renounced.
+     * @param _account Must equal msg.sender. Used as an additional check.
+     */
+    function renounceRole(bytes32 _role, address _account) public override(IAccessControlUpgradeable, AccessControlUpgradeable) {
+        if (_role == DEFAULT_ADMIN_ROLE && getRoleMemberCount(_role) == 1) {
+            revert MustHaveOneRoleAdmin();
+        }
+        super.renounceRole(_role, _account);
     }
 
     /**
@@ -72,7 +126,7 @@ contract StakeHolder is AccessControlEnumerableUpgradeable, UUPSUpgradeable {
         if (msg.value == 0) {
             revert MustStakeMoreThanZero();
         }
-        addStake(msg.sender, msg.value);
+        _addStake(msg.sender, msg.value, false);
     }
 
     /**
@@ -82,12 +136,13 @@ contract StakeHolder is AccessControlEnumerableUpgradeable, UUPSUpgradeable {
      * @param _amountToUnstake Amount of stake to remove.
      */
     function unstake(uint256 _amountToUnstake) external {
-        uint256 currentStake = balances[msg.sender];
+        StakeInfo storage stakeInfo = balances[msg.sender];
+        uint256 currentStake = stakeInfo.stake;
         if (currentStake < _amountToUnstake) {
             revert UnstakeAmountExceedsBalance(_amountToUnstake, currentStake);
         }
         uint256 newBalance = currentStake - _amountToUnstake;
-        balances[msg.sender] = newBalance;
+        stakeInfo.stake = newBalance;
 
         payable(msg.sender).transfer(_amountToUnstake);
 
@@ -99,10 +154,10 @@ contract StakeHolder is AccessControlEnumerableUpgradeable, UUPSUpgradeable {
      * @dev The total amount to distribute must match msg.value.
      *  This function does not need re-entrancy guard as the distribution mechanism 
      *  does not call out to another contract.
-     * @param _recipients An array of recipients to distribute value to.
-     * @param _amounts An array of amounts to be distributed to each recipient.
+     * @param _recipientsAndAmounts An array of recipients to distribute value to and 
+     *          amounts to be distributed to each recipient.
      */
-    function distributeRewards(address[] calldata _recipients, uint256[] calldata _amounts)
+    function distributeRewards(AccountAmount[] calldata _recipientsAndAmounts)
         external
         payable
     {
@@ -110,16 +165,16 @@ contract StakeHolder is AccessControlEnumerableUpgradeable, UUPSUpgradeable {
         if (msg.value == 0) {
             revert MustDistributeMoreThanZero();
         }
-        uint256 len = _recipients.length;
-        if (len != _amounts.length) {
-            revert RecipientsAmountsMismatch(len, _amounts.length);
-        }
+        uint256 len = _recipientsAndAmounts.length;
 
         // Distribute the value.
         uint256 total = 0;
         for (uint256 i = 0; i < len; i++) {
-            uint256 amount = _amounts[i];
-            addStake(_recipients[i], amount);
+            AccountAmount calldata accountAmount = _recipientsAndAmounts[i];
+            uint256 amount = accountAmount.amount;
+            // Add stake, but require the acount to either currently be staking or have
+            // previously staked. 
+            _addStake(accountAmount.account, amount, true);
             total += amount;
         }
 
@@ -136,16 +191,23 @@ contract StakeHolder is AccessControlEnumerableUpgradeable, UUPSUpgradeable {
      * @return _balance The balance of the account.
      */
     function getBalance(address _account) external view returns (uint256 _balance) {
-        return balances[_account];
+        return balances[_account].stake;
+    }
+
+    /**
+     * @notice Determine if an account has ever staked.
+     * @param _account The account to determine if they have staked
+     * @return _everStaked True if the account has ever staked.
+     */
+    function hasStaked(address _account) external view returns (bool _everStaked) {
+        return balances[_account].hasStaked;
     }
 
     /**
      * @notice Get the length of the stakers array.
-     * @dev This will be greater than or equal to the number of staker accounts that have
-     *  ever existed. Some of the accounts might have a zero balance, having staked and then
-     *  unstaked. Some accounts could be in the array two or more times, having staked, unstaked,
-     *  and then staked again. Another scenario could be an account staking, unstaking, and then having
-     *  a distribution paid.
+     * @dev This will be equal to the number of staker accounts that have ever staked. 
+     *  Some of the accounts might have a zero balance, having staked and then
+     *  unstaked. 
      * @return _len The length of the stakers array.
      */
     function getNumStakers() external view returns (uint256 _len) {
@@ -181,14 +243,20 @@ contract StakeHolder is AccessControlEnumerableUpgradeable, UUPSUpgradeable {
      * @dev If the account has a zero balance prior to this call, add the account to the stakers array.
      * @param _account Account to add stake to.
      * @param _amount The amount of stake to add.
+     * @param _existingAccountsOnly If true, revert if the account has never been used.
      */
-    function addStake(address _account, uint256 _amount) private {
-        uint256 currentStake = balances[_account];
-        if (currentStake == 0) {
+    function _addStake(address _account, uint256 _amount, bool _existingAccountsOnly) private {
+        StakeInfo storage stakeInfo = balances[_account];
+        uint256 currentStake = stakeInfo.stake;
+        if (!stakeInfo.hasStaked) {
+            if (_existingAccountsOnly) {
+                revert AttemptToDistributeToNewAccount(_account, _amount);
+            }
             stakers.push(_account);
+            stakeInfo.hasStaked = true;
         }
         uint256 newBalance = currentStake + _amount;
-        balances[_account] = newBalance;
+        stakeInfo.stake = newBalance;
         emit StakeAdded(_account, _amount, newBalance);
     }
 
